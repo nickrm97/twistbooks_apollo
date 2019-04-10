1719b7f77cd8a32ad59f98c3addcd3f292bdae4c
nickrm97 <nicholasrmills@icloud.com>
2019-04-10 16:07:25 +1000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// Apollo stuff
+import ApolloClient from "apollo-boost";
+import { Query, ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "https://api.twistbooks.com/graphql"
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ApolloProvider client={client}>
+          <header className="App-header">
+            <div className="App-logo">
+              <h2>My first Apollo app 🚀</h2>
+            </div>
+          </header>
+        </ApolloProvider>
       </div>
     );
   }
